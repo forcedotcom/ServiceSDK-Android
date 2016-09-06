7db12147b85efa0a1504e94c8df4b0751f5ee418
Yuvi <yzalkow@salesforce.com>
2016-09-06 10:14:44 -0700

    Added KB 0.3.0 Reference Documentation

---
+++ 0.3.0/knowledge-ui-javadoc/javadoc/assets/navtree_data.js
@@ -0,0 +1,8 @@
+var NAVTREE_DATA =
+[ [ "com.salesforce.android.knowledge.ui", "com/salesforce/android/knowledge/ui/package-summary.html", [ [ "Interfaces", null, [ [ "KnowledgeImageProvider", "com/salesforce/android/knowledge/ui/KnowledgeImageProvider.html", null, "" ], [ "KnowledgeUIClient", "com/salesforce/android/knowledge/ui/KnowledgeUIClient.html", null, "" ], [ "KnowledgeUIClient.OnCloseListener", "com/salesforce/android/knowledge/ui/KnowledgeUIClient.OnCloseListener.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "KnowledgeUI", "com/salesforce/android/knowledge/ui/KnowledgeUI.html", null, "" ], [ "KnowledgeUIConfiguration", "com/salesforce/android/knowledge/ui/KnowledgeUIConfiguration.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
