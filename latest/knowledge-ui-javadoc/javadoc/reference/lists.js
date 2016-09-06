4636927cc9dcdd05ad20739255a08f127433406b
Yuvi <yzalkow@salesforce.com>
2016-09-06 10:25:55 -0700

    added latest folder

---
+++ latest/knowledge-ui-javadoc/javadoc/reference/lists.js
@@ -0,0 +1,9 @@
+var DATA = [
+      { id:0, label:"com.salesforce.android.knowledge.ui", link:"com/salesforce/android/knowledge/ui/package-summary.html", type:"package" },
+      { id:1, label:"com.salesforce.android.knowledge.ui.KnowledgeImageProvider", link:"com/salesforce/android/knowledge/ui/KnowledgeImageProvider.html", type:"class" },
+      { id:2, label:"com.salesforce.android.knowledge.ui.KnowledgeUI", link:"com/salesforce/android/knowledge/ui/KnowledgeUI.html", type:"class" },
+      { id:3, label:"com.salesforce.android.knowledge.ui.KnowledgeUIClient", link:"com/salesforce/android/knowledge/ui/KnowledgeUIClient.html", type:"class" },
+      { id:4, label:"com.salesforce.android.knowledge.ui.KnowledgeUIClient.OnCloseListener", link:"com/salesforce/android/knowledge/ui/KnowledgeUIClient.OnCloseListener.html", type:"class" },
+      { id:5, label:"com.salesforce.android.knowledge.ui.KnowledgeUIConfiguration", link:"com/salesforce/android/knowledge/ui/KnowledgeUIConfiguration.html", type:"class" }
+
+    ];
