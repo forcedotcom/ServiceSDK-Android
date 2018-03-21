3c2fbe58007dd32ac81378c24b46937fbcd4e5f6
riley-macdonald <riley.macdonald@salesforce.com>
2018-03-21 11:38:53 -0300

    W-4824894: Add build variants kotlinImplDebug and javaImplDebug

---
+++ Examples/SnapinsSDKKotlinExample/app/src/javaImplDebug/java/com/salesforce/snapinssdkexample/activities/settings/KnowledgeSettingsActivity.java
@@ -0,0 +1,10 @@
+package com.salesforce.snapinssdkexample.activities.settings;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by riley.macdonald on 3/21/18.
+ */
+
+public class KnowledgeSettingsActivity extends AppCompatActivity {
+}
